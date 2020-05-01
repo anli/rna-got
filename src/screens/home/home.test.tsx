0c8b4f7f1d73a2a85899639f4a6b7341e7237e1d
Su Anli <su_anli@popularworld.com>
2020-05-01 18:38:24 +0800

    test(home.test): add clean up to test

--- src/screens/home/home.test.tsx
+++ src/screens/home/home.test.tsx
@@ -1,22 +1,28 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {HomeScreen} from '@screens';
 import {store} from '@store';
 import React from 'react';
 import 'react-native';
-import {fireEvent, render} from 'react-native-testing-library';
+import {
+  cleanup,
+  fireEvent,
+  render as RNTRender,
+} from 'react-native-testing-library';
 import * as redux from 'react-redux';
 import {Provider as StoreProvider} from 'react-redux';
-import HomeScreen from './home';
 
-const getComponent = () =>
-  render(
+const render = (children: Element) =>
+  RNTRender(
     <StoreProvider store={store}>
-      <NavigationContainer>
-        <HomeScreen />
-      </NavigationContainer>
+      <NavigationContainer>{children}</NavigationContainer>
     </StoreProvider>,
   );
 
 describe('Home Screen', () => {
+  afterAll(() => {
+    cleanup();
+  });
+
   it('Given any, When I am at "Home Screen", And data is "Loading", Then I should see "Placeholder List"', async () => {
     const spy = jest.spyOn(redux, 'useSelector');
     spy.mockReturnValue({
@@ -28,7 +34,7 @@ describe('Home Screen', () => {
       },
     });
 
-    const component = getComponent();
+    const component = render(<HomeScreen />);
 
     expect(component.getByTestId('HomeScreen.PlaceholderList')).toBeDefined();
 
@@ -50,7 +56,7 @@ describe('Home Screen', () => {
     const mockDispatch = jest.fn();
     spyUseDispatch.mockReturnValue(mockDispatch);
 
-    const component = getComponent();
+    const component = render(<HomeScreen />);
 
     expect(mockDispatch).toBeCalledTimes(1);
 
@@ -63,14 +69,14 @@ describe('Home Screen', () => {
   });
 
   it('Given any, When I am at "Home Screen", Then I should see "List of Characters Names"', async () => {
-    const component = getComponent();
+    const component = render(<HomeScreen />);
     expect(component.getByText('John')).toBeDefined();
     expect(component.getByText('Mary')).toBeDefined();
     expect(component.getByText('Jane')).toBeDefined();
   });
 
   it('Given any, When I am at "Home Screen", And I scroll to "bottom", Then I should see "Additional List of Characters Names"', async () => {
-    const component = getComponent();
+    const component = render(<HomeScreen />);
 
     fireEvent(component.getByTestId('HomeScreen.FlatList'), 'onEndReached');
 
