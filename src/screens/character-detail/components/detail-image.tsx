7cec08c075641b92b26825e51ac5fdc5654c3c5c
Su Anli <anli@users.noreply.github.com>
2020-05-03 14:03:59 +0800

    feat(detail-image): add placeholder image on loading (#8)

--- src/screens/character-detail/components/detail-image.tsx
+++ src/screens/character-detail/components/detail-image.tsx
@@ -1,28 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useWindowDimensions} from 'react-native';
 import FastImage from 'react-native-fast-image';
+import {Fade, Placeholder, PlaceholderMedia} from 'rn-placeholder';
 import styled from 'styled-components/native';
 
 const DetailImage = ({url}: {url: string}) => {
+  const [isLoading, setIsLoading] = useState(true);
   const imageWidth = Math.floor(useWindowDimensions().width);
   const imageHeight = Math.floor(useWindowDimensions().height * 0.5);
 
+  /* istanbul ignore next */
+  const onLoading = () => setIsLoading(true);
+
+  /* istanbul ignore next */
+  const onLoaded = () => setIsLoading(false);
+
   return (
-    <Image
-      height={imageHeight}
-      width={imageWidth}
-      source={{
-        uri: `${url}/${imageWidth}/${imageHeight}`,
-        priority: FastImage.priority.normal,
-      }}
-      resizeMode={FastImage.resizeMode.cover}
-    />
+    <>
+      <Image
+        height={imageHeight}
+        width={imageWidth}
+        source={{
+          uri: `${url}/${imageWidth}/${imageHeight}`,
+          priority: FastImage.priority.normal,
+        }}
+        resizeMode={FastImage.resizeMode.cover}
+        onLoadStart={onLoading}
+        onLoadEnd={onLoaded}
+      />
+      {isLoading && (
+        <Placeholder Animation={Fade}>
+          <PlaceholderImage height={imageHeight} width={imageWidth} />
+        </Placeholder>
+      )}
+    </>
   );
 };
 
 export default DetailImage;
 
 const Image = styled(FastImage)<{height: string; width: string}>`
+  height: ${props => props.height}px;
+  width: ${props => props.width}px;
+`;
+
+const PlaceholderImage = styled(PlaceholderMedia)<{
+  height: string;
+  width: string;
+}>`
+  top: -${props => props.height}px;
+  position: absolute;
   height: ${props => props.height}px;
   width: ${props => props.width}px;
 `;
